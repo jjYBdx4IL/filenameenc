a59b00b95464b524c9b387f7cc251cd1ee215a34
Mark <github880520@mstier.de>
2014-04-04 17:15:40 +0200

    migrate to maven, include java.nio solution

--- src/main/java/filenameenc/Test.java
+++ src/main/java/filenameenc/Test.java
@@ -1,4 +1,4 @@
-// vim:set shiftwidth=4 tabstop=4 expandtab ai smartindent fileformat=unix fileencoding=utf-8:
+package filenameenc;
 
 /**
  * Beware! You need to make sure that your specified encodings actually exist
@@ -27,20 +27,25 @@
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Iterator;
 import java.util.Locale;
 
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String encUsed = Charset.defaultCharset().displayName();
         System.out.println(args[0] + ", charset in use: " + encUsed);
+        String outdir = args[1];
 
         if (args[0].equals("create")) {
             if (encUsed.indexOf("ISO") == -1) {
                 throw new RuntimeException();
             }
             try {
-                File f = new File("a/üöä");
+                File f = new File(outdir+File.separatorChar+"üöä");
                 f.delete();
                 f.createNewFile();
             } catch (Exception ex) {
@@ -50,13 +55,37 @@ public class Test {
             if (encUsed.indexOf("UTF") == -1) {
                 throw new RuntimeException();
             }
-            for (File f : new File("a").listFiles()) {
+            
+            int fileNotFoundOld = 0;
+            for (File f : new File(outdir).listFiles()) {
                 if (!f.exists()) {
                     System.out.println("NOT FOUND: " + f.getPath());
+                    fileNotFoundOld++;
                 } else {
                     System.out.println("found:     " + f.getPath());
                 }
             }
+
+            int fileNotFoundNew = 0;
+            DirectoryStream<Path> ds = Files.newDirectoryStream(new File(outdir).toPath());
+            for(Iterator<Path> it = ds.iterator();it.hasNext();) {
+                Path p = it.next();
+                if (!Files.exists(p)) {
+                    fileNotFoundNew++;
+                    System.out.println("NOT FOUND NEW: " + p);
+                } else {
+                    System.out.println("new found:     " + p);
+                }
+            }
+
+            /**
+             * Result: old java.io.* API cannot find badly encoded filenames, but the new java.nio.* can!
+             */
+            if(fileNotFoundOld != 1 || fileNotFoundNew != 0) {
+                throw new RuntimeException("test failed");
+            }
         }
     }
 }
+
+// vim:set shiftwidth=4 tabstop=4 expandtab ai smartindent fileformat=unix fileencoding=utf-8:
